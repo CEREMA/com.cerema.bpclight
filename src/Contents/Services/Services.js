fdfb4c517ef64730b95c553b57567500461ef468
Stéphane Zucatti <stephane.zucatti@me.com>
2015-01-30 15:07:24 +0100

    Update# 12

--- src/Contents/Services/Services.js
+++ src/Contents/Services/Services.js
@@ -3,9 +3,9 @@ Services = {
 	getAll: function(o,cb) {
 		var db=Services.using('db');
 		if (o.kuni) 
-		db.model('bpclight','select * from subdis where archive=0 and kuni='+o.kuni+' order by LibSub',cb);
+		db.model('bpclight','select * from subdis where archive=0 and kuni='+o.kuni+' order by LibSubc',cb);
 		else
-		db.model('bpclight','select * from subdis where archive=0 order by LibSub',cb);
+		db.model('bpclight','select * from subdis where archive=0 order by LibSubc',cb);
 	}
 }
 
