896c43bfbbf8accb46e6e72ca12918db7d77840b
Stéphane Zucatti <stephane.zucatti@me.com>
2015-01-30 15:41:35 +0100

    Update# 17

--- src/Contents/Services/Formation.js
+++ src/Contents/Services/Formation.js
@@ -6,8 +6,8 @@ Formation = {
 	
 	getAll2: function(o,cb) {
 		var db=Formation.using('db');
-		db.model('bpclight','SELECT * FROM Recapitulatif, Type_formation, frequence WHERE Recapitulatif.type_formation=Type_formation.id_formation AND Recapitulatif.Kage ="'+o.Kage+'" ORDER BY 3 ',cb);
-		// AND Recapitulatif.Kage ="'+o.Kage+'"
+		console.log('SELECT * FROM Recapitulatif, Type_formation, frequence WHERE Recapitulatif.type_formation=Type_formation.id_formation AND Recapitulatif.Kage ="'+o.Kage+'" ORDER BY 3');
+		db.model('bpclight','SELECT * FROM Recapitulatif, Type_formation, frequence WHERE Recapitulatif.type_formation=Type_formation.id_formation AND Recapitulatif.Kage ="'+o.Kage+'" ORDER BY 3 ',cb);		
 	},
 	del: function(o,cb)
 	{
@@ -18,9 +18,8 @@ Formation = {
 	{
 		var db=Formation.using('db');
 		//console.log('insert into Recapitulatif(Kage,Formations,Date,Session,Frequence,Nom_organisme) values ("'+o.Kage+'","'+o.Formations+'","'+o.Date+'","'+"a"+'","'+"g"+'","'+o.Nom_organisme+'");
-		db.query('bpclight','insert into Recapitulatif (Kage,Date,Session,Nom_organisme,type_formation) values ("'+o.Kage+'","'+o.Date+'","'+o.Session+'","'+o.Nom_organisme+'","'+o.Formations+'")',cb);
-	
-	console.log(cb);
+		db.query('bpclight','insert into Recapitulatif (Kage,Date,Session,Nom_organisme,type_formation) values ("'+o.Kage+'","'+o.Date+'","'+o.Session+'","'+o.Nom_organisme+'","'+o.Formations+'")',cb);	
+		console.log(cb);
 	}, 
 	test_delete: function(o,cb)
 	{
