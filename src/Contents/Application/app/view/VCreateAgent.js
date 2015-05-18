e07cf6c53059f558203184d415d554783ffed44d
Stéphane Zucatti <stephane.zucatti@me.com>
2015-05-18 11:20:36 +0200

    Update# 179

--- src/Contents/Application/app/view/VCreateAgent.js
+++ src/Contents/Application/app/view/VCreateAgent.js
@@ -5,7 +5,7 @@ App.view.define('VCreateAgent', {
     alias: 'widget.createAgent',
     initComponent: function() {
 		
-		this.width = 440;
+		this.width = 430;
         this.height = 660;
         this.title = "Nouvel agent";
 
