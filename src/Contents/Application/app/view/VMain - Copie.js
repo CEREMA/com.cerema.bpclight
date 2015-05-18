10dc8147740a2fb5f8c23c2a849cb0497fb9ed37
Stéphane Zucatti <stephane.zucatti@me.com>
2015-05-18 08:46:53 +0200

    Update# 122

---
+++ src/Contents/Application/app/view/VMain - Copie.js
@@ -0,0 +1,36 @@
+App.view.define('VMain', {
+
+    extend: 'Ext.Panel',
+	alias : 'widget.mainform',
+	border: false,
+	
+	layout: "border",
+	
+	items: [
+		{
+			region: 'north',
+			height: 25,
+			minHeight: 25,
+			border:false,
+			baseCls: 'cls-header',
+			xtype: "Menu",
+			itemId: "MenuPanel",
+			menu: [
+				{
+					text: "Agent",
+					menu: [
+						{
+							text: "Nouveau",
+							itemId: "MNU_AGENT_NEW"
+						}
+					]
+				}
+			]		
+		},
+		{
+			region: "center",
+			xtype: "TPrincipal"
+		}
+	]
+	
+});
