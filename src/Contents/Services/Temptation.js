d803009f9d6bce0739277f0889f1ec6d6985bdf7
XXX <xxx@xxx.com>
2015-05-19 16:34:20 +0200

    Update# 297

--- src/Contents/Services/Temptation.js
+++ src/Contents/Services/Temptation.js
@@ -2,11 +2,7 @@ Temptation = {
 	search: function(str,cb)
 	{
 		var db=Temptation.using('db');
-		var matri=[];
-		db.query('bpclight','SELECT matri FROM agents WHERE actif=1',function(e,o) {
-			for (var i=0;i<o.length;i++) matri.push(o[i].matri);
-			db.query('temptation',"SELECT DISTINCT Hm.MATRI matri, Hm.NOMPRE nompre FROM HopeMpl Hm WHERE Hm.MATRI not in ('"+matri.join('\',\'')+"')",cb);
-		});		
+		db.query('temptation',"SELECT DISTINCT Hm.MATRI matri, Hm.NOMPRE nompre FROM HopeMpl Hm WHERE Hm.NomPre LIKE '%"+str+"%'",cb);
 	}
 };
 
