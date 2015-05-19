17ad48118e65f87214c52a719aafc8038a5d3b4a
XXX <xxx@xxx.com>
2015-05-19 14:00:11 +0200

    Update# 257

--- src/Contents/Services/Temptation.js
+++ src/Contents/Services/Temptation.js
@@ -2,6 +2,7 @@ Temptation = {
 	search: function(str,cb)
 	{
 		var db=Temptation.using('db');
+		console.log('SELECT DISTINCT Hm.MATRI, Hm.NOMPRE FROM HopeMpl Hm WHERE Hm.NomPre LIKE "%'+str+'%"');
 		db.query('temptation','SELECT DISTINCT Hm.MATRI, Hm.NOMPRE FROM HopeMpl Hm WHERE Hm.NomPre LIKE "%'+str+'%"',cb);
 	}
 };
