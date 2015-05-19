1bdcfd7d57178316e75699c23418618bdaf455f9
XXX <xxx@xxx.com>
2015-05-19 15:06:47 +0200

    Update# 276

--- src/Contents/Services/Temptation.js
+++ src/Contents/Services/Temptation.js
@@ -2,7 +2,7 @@ Temptation = {
 	search: function(str,cb)
 	{
 		var db=Temptation.using('db');
-		db.query('temptation',"SELECT DISTINCT Hm.MATRI, Hm.NOMPRE FROM HopeMpl Hm WHERE Hm.NomPre LIKE '%"+str+"%'",cb);
+		db.query('temptation',"SELECT DISTINCT Hm.MATRI matri, Hm.NOMPRE nompre FROM HopeMpl Hm WHERE Hm.NomPre LIKE '%"+str+"%'",cb);
 	}
 };
 
