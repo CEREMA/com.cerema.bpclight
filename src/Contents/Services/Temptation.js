a69db03442304e9bf3142ab7b7991136b0b216dd
XXX <xxx@xxx.com>
2015-05-19 11:25:34 +0200

    Update# 255

---
+++ src/Contents/Services/Temptation.js
@@ -0,0 +1,9 @@
+Temptation = {
+	search: function(str,cb)
+	{
+		var db=Temptation.using('db');
+		db.query('temptation','SELECT DISTINCT Hm.MATRI, Hm.NOMPRE FROM HopeMpl Hm WHERE Hm.NomPre LIKE "%'+str+'%"',cb);
+	}
+};
+
+module.exports = Temptation;
