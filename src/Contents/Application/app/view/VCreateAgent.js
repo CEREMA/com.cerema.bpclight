d576a8299c942fc0955a1e0ea935c62f04e02d2e
Stéphane Zucatti <stephane.zucatti@me.com>
2015-05-20 10:02:54 +0200

    Update# 354

--- src/Contents/Application/app/view/VCreateAgent.js
+++ src/Contents/Application/app/view/VCreateAgent.js
@@ -157,7 +157,7 @@ App.view.define('VCreateAgent', {
 							margin: {
 								left: 10
 							},
-							width: 250
+							width: 240
 						},
 						{
 							xtype: "combo",
@@ -172,7 +172,7 @@ App.view.define('VCreateAgent', {
 							margin: {
 								left: 10
 							},
-							width: 250
+							width: 240
 						}					
 					]
 				}				
