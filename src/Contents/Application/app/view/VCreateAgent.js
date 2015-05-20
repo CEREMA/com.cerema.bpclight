178895a74d9ae9faf65956d2f0d0b19b7f373598
Stéphane Zucatti <stephane.zucatti@me.com>
2015-05-20 09:12:36 +0200

    Update# 328

--- src/Contents/Application/app/view/VCreateAgent.js
+++ src/Contents/Application/app/view/VCreateAgent.js
@@ -76,7 +76,7 @@ App.view.define('VCreateAgent', {
 				},
 				padding: 0,
 				columns: [
-					{ text: 'Matricule',  dataIndex: 'matri' },
+					{ text: 'Matricule',  dataIndex: 'matri',width: 80 },
 					{ text: 'Agent', dataIndex: 'nompre', flex: 1 },
 					{ text: 'Ouverture', dataIndex: 'datouv' }
 				],
