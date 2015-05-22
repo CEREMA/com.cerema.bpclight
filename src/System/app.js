e1305edb601c77931db334e2efe8ce14d5c3524d
Stéphane Zucatti <stephane.zucatti@me.com>
2015-05-22 09:00:02 +0200

    Update# 374

--- src/System/app.js
+++ src/System/app.js
@@ -46,11 +46,10 @@ App = {
 			if (req.body.kage) {
 				var o=req.body.kage.split(',');
 				App.Agents.exportXLS(o,function(e,tabs) {
-					console.log(e);
-					console.log(tabs);
+
 					var uid=Math.uuid();
 					var workbook = excelbuilder.createWorkbook(__dirname+require('path').sep+'tmp', uid+'.xlsx');
-					var sheet1 = workbook.createSheet('BPCLight', 500, 1500);
+					var sheet1 = workbook.createSheet('BPCLight', 1500, 1500);
 					var conf={};
 					conf.cols = [
 					{
