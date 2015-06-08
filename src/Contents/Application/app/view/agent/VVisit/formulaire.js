26da1032285ea610193ae2778e1cf7ab65af161e
Stéphane Zucatti <stephane.zucatti@me.com>
2015-06-08 11:25:46 +0200

    Update# 397

---
+++ src/Contents/Application/app/view/agent/VVisit/formulaire.js
@@ -0,0 +1,14 @@
+App.view.define('agent.VVisit.formulaire', {
+	alias: "widget.TVisitData",
+	extend: "Ext.Panel",
+	initComponent: function()
+	{
+		this.title="Visite médicale";
+		this.layout="vbox";
+		this.border=false;
+		this.items = [
+		
+		];
+		this.callParent();
+	}
+});
