e484ef3dab0908d740ec44e03d78b504a6ad9394
Stéphane Zucatti <stephane.zucatti@me.com>
2015-06-08 13:39:04 +0200

    Update# 438

--- src/Contents/Application/app/view/agent/VVisit/formulaire.js
+++ src/Contents/Application/app/view/agent/VVisit/formulaire.js
@@ -4,7 +4,7 @@ App.view.define('agent.VVisit.formulaire', {
     initComponent: function() {
 		this.title="Visite médicale";
         this.width = 800;
-        this.height = 760;
+        this.height = 780;
 
         this.layout = {
             type: 'vbox'
