65e18c76fc2c2a98904aa1404fc3f48e81756924
Stéphane Zucatti <stephane.zucatti@me.com>
2015-06-08 10:02:56 +0200

    Update# 379

--- src/Contents/Application/app/view/agent/VVisit.js
+++ src/Contents/Application/app/view/agent/VVisit.js
@@ -15,6 +15,12 @@ App.view.define('agent.VVisit', {
 			labelAlign: "top",
 			height: 250,
 			padding: 10
+		},
+		{
+			xtype: "grid",
+			itemId: "grid_medic",
+			width: "100%",
+			flex: 1
 		}
 		];
 		this.callParent();
