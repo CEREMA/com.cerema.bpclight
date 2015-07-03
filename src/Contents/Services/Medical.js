256de0e154b70892308d8e3da26ec9e67fde399a
Stéphane Zucatti <stephane.zucati@me.com>
2015-07-03 10:11:21 +0200

    Update# 501

--- src/Contents/Services/Medical.js
+++ src/Contents/Services/Medical.js
@@ -1,8 +1,7 @@
 Medical={
 	getAll: function()
 	{
-		var db=Medical.using('db');
-		db.model('bpclight','select medic_dossiers.id,date,medic_type.type,medic_cat.cat from medic_dossiers join medic_type on medic_type.id=medic_dossiers.type join medic_cat on medic_cat.id=medic_dossiers.cat where kage=614',cb);
+		Medical.using('db').model('bpclight','select medic_dossiers.id,date,medic_type.type,medic_cat.cat from medic_dossiers join medic_type on medic_type.id=medic_dossiers.type join medic_cat on medic_cat.id=medic_dossiers.cat where kage=614',cb);
 	}
 };
 
