214babbe798d8d9d854cfb16d4d15d4017be4993
Stéphane Zucatti <stephane.zucati@me.com>
2015-07-03 10:17:59 +0200

    Update# 503

--- src/Contents/Services/Medical.js
+++ src/Contents/Services/Medical.js
@@ -1,5 +1,5 @@
 Medical={
-	getAll: function()
+	getAll: function(o,cb)
 	{
 		Medical.using('db').model('bpclight','select medic_dssiers.id,date,medic_type.type,medic_cat.cat from medic_dossiers join medic_type on medic_type.id=medic_dossiers.type join medic_cat on medic_cat.id=medic_dossiers.cat where kage=614',cb);
 	}
