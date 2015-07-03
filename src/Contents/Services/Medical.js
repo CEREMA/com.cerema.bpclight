bed39cf105647f3c16516b4efe2d9dacbcc5379e
Stéphane Zucatti <stephane.zucati@me.com>
2015-07-03 10:00:17 +0200

    Update# 494

--- src/Contents/Services/Medical.js
+++ src/Contents/Services/Medical.js
@@ -1,12 +1,5 @@
 Medical={
-	getTypes: function(o,cb)
-	{
-		Medical.using('db').model('bpclight','select * from medic_types',cb);
-	},
-	getCats: function(o,cb)
-	{
-		Medical.using('db').model('bpclight','select * from medic_cats',cb);
-	}
+
 };
 
 module.exports=Medical;
