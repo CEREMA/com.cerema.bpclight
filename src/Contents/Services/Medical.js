9784f004069568571226dc74da0077db12d6c55e
Stéphane Zucatti <stephane.zucati@me.com>
2015-07-03 10:09:03 +0200

    Update# 497

--- src/Contents/Services/Medical.js
+++ src/Contents/Services/Medical.js
@@ -1,5 +1,9 @@
 Medical={
-
+	getAll: function()
+	{
+		var db=Medical.using('db');
+		db.model('bpclight','select medic_dossiers.id,date,medic_type.type,medic_cat.cat from medic_dossiers join medic_type on medic_type.type=medic_dossiers.id join medic_cat on medic_cat.id=medic_dossiers.cat where kage=614',cb);
+	}
 };
 
 module.exports=Medical;
