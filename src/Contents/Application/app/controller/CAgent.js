64185aceffc05a990361b5f9bfbaeee1293c7bed
Stéphane Zucatti <stephane.zucatti@me.com>
2015-07-20 13:28:03 +0200

    Update# 572

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -159,7 +159,6 @@ App.controller.define('CAgent', {
 	},
 	Visit_onRecord: function(p)
 	{
-		alert(p.up('window')._id);
 		var RG = App.get( 'TVisitData propertygrid#RG' ).getSource();
 		var EFR = App.get( 'TVisitData propertygrid#EFR' ).getSource();
 		var TU = App.get( 'TVisitData propertygrid#TU' ).getSource();
