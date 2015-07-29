08ebc4e4fea4293f4164571125b1fe6f884bea9b
Stéphane Zucatti <stephane.zucati@me.com>
2015-07-29 15:05:04 +0200

    Update# 674

--- src/System/app.js
+++ src/System/app.js
@@ -45,89 +45,91 @@ App = {
 			var excelbuilder=App.using('msexcel-builder');
 			if (req.body.kage) {
 				var o=req.body.kage.split(',');
-				App.Agents.exportXLS(o,function(e,tabs) {
-
-					var uid=Math.uuid();
-					var workbook = excelbuilder.createWorkbook(__dirname+require('path').sep+'tmp', uid+'.xlsx');
-					var sheet1 = workbook.createSheet('BPCLight', 1500, 1500);
-					var conf={};
-					conf.cols = [
-					{
-						caption: 'Nom',
-						type:'string',
-						width: 50
-					},
-					{
-						caption: 'Prénom',
-						type:'string',
-						width: 50
-					},
-					{
-						caption: 'Date naissance',
-						type:'date',
-						width: 30
-					},
-					{
-						caption: 'Date position',
-						type:'date',
-						width: 30
-					},
-					{
-						caption: 'Grade',
-						type:'string',
-						width: 50
-					},
-					{
-						caption: 'Département',
-						type:'string',
-						width: 100
-					},
-					{
-						caption: 'Service',
-						type:'string',
-						width: 100
-					},
-					{
-						caption: 'Arrivée',
-						type:'string',
-						width: 100
-					},
-					{
-						caption: 'Motif',
-						type:'string',
-						width: 100
-					},
-					{
-						caption: 'Position',
-						type:'string',
-						width: 100
-					},
-					{
-						caption: 'Service destination',
-						type:'string',
-						width: 100
-					}
-					];			
-					for (var e=0;e<conf.cols.length;e++) {
-						sheet1.set(e+1,1,conf.cols[e].caption);
-						sheet1.width(e+1, conf.cols[e].width*1);
-					};
-					for (var i=0;i<tabs.length;i++) {
-						var element=tabs[i];
-						var k=1;
-						var ii=i+2;
-						for (var el in element) {
-							if (k<12) {
-								sheet1.set(k, ii, element[el]);								
-							};
-							k++;
-						};
-					};			
-					workbook.save(function(ok){
-						res.end('/tmp/'+uid+'.xlsx');
-					});					
+				if (req.body.name=="Civility") {
+					App.Agents.exportCiv(o,function(e,tabs) {
+						
+						var uid=Math.uuid();
+						var workbook = excelbuilder.createWorkbook(__dirname+require('path').sep+'tmp', uid+'.xlsx');
+						var sheet1 = workbook.createSheet('BPCLight', 1500, 1500);
+						var conf={};
+						conf.cols = [
+						{
+							caption: 'Nom',
+							type:'string',
+							width: 50
+						},
+						{
+							caption: 'Prénom',
+							type:'string',
+							width: 50
+						},
+						{
+							caption: 'Date naissance',
+							type:'date',
+							width: 30
+						},
+						{
+							caption: 'Date position',
+							type:'date',
+							width: 30
+						},
+						{
+							caption: 'Grade',
+							type:'string',
+							width: 50
+						},
+						{
+							caption: 'Département',
+							type:'string',
+							width: 100
+						},
+						{
+							caption: 'Service',
+							type:'string',
+							width: 100
+						},
+						{
+							caption: 'Arrivée',
+							type:'string',
+							width: 100
+						},
+						{
+							caption: 'Motif',
+							type:'string',
+							width: 100
+						},
+						{
+							caption: 'Position',
+							type:'string',
+							width: 100
+						},
+						{
+							caption: 'Service destination',
+							type:'string',
+							width: 100
+						}
+						];	
 					
-				});
+						for (var e=0;e<conf.cols.length;e++) {
+							sheet1.set(e+1,1,conf.cols[e].caption);
+							sheet1.width(e+1, conf.cols[e].width*1);
+						};
+						for (var i=0;i<tabs.length;i++) {
+							var element=tabs[i];
+							var k=1;
+							var ii=i+2;
+							for (var el in element) {
+								if (k<12) {
+									sheet1.set(k, ii, element[el]);								
+								};
+								k++;
+							};
+						};			
+						workbook.save(function(ok){
+							res.end('/tmp/'+uid+'.xlsx');
+						});					
+					});	
+				};
 			};
 		});		
 		app.post('/agent.mail',function(req,res) {
