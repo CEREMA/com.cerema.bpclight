f128b8c22e675132edc964ba84dc35ce12f1f825
Stéphane Zucatti <stephane.zucati@me.com>
2015-07-30 15:39:43 +0200

    Update# 731

--- src/Contents/Services/Medical.js
+++ src/Contents/Services/Medical.js
@@ -3,14 +3,6 @@ Medical={
 	{
 		Medical.using('db').model('bpclight','select medic_dossiers.id,date,medic_type.type,medic_cat.cat from medic_dossiers join medic_type on medic_type.id=medic_dossiers.type join medic_cat on medic_cat.id=medic_dossiers.cat where kage='+o.kage+' order by medic_dossiers.date desc',cb);
 	},
-	getLibelle: function(o,cb)
-	{
-		Medical.using('db').model('bpclight','select libelle from medic_gen where kage='+o,cb);
-	},
-	setLibelle: function(o,cb)
-	{
-		Medical.using('db').post('bpclight','medic_gen',o,cb);
-	},
 	getDossierById: function(o,cb)
 	{
 		console.log('select * from medic_dossiers where id='+o);
