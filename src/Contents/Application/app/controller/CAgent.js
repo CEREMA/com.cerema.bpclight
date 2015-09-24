5a0c6adbf1313e04df085e48fa3f446c60595b73
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2015-09-24 15:40:39 +0200

    Update# 830

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -167,7 +167,7 @@ App.controller.define('CAgent', {
 	{
 		App.DB.get('bpclight://medic_dossiers{kage,date}?id='+p.up('window')._id,function(o) {
 			// get Kage
-			App.DB.get('bpclight://medic_dossiers{kage,date}?DATE(date)='+o.data[0].date.split('T')[0]+'&kage='+o.data[0].kage,function(m) {
+			App.DB.get('bpclight://medic_dossiers{kage,date}?DATE(date)<="'+o.data[0].date.split('T')[0]+'"&kage='+o.data[0].kage,function(m) {
 				console.log(m);
 			});
 			console.log(o);		
