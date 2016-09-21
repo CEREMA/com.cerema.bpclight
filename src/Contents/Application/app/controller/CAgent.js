ee75f2b6dcd4174d68c9121cc3a94b6f237a23f2
Stéphane Zucatti <stephane.zucatti@me.com>
2016-09-21 11:24:21 +0200

    Update# 959

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -782,6 +782,7 @@ App.controller.define('CAgent', {
 	},
 	position_onchange: function(p,record)
 	{
+		console.log(record);
 		this.situation_cancel_onclick(p);
 		App.get(p.up('TSituation'),'grid#gridPositions').hide();
 		App.get(p.up('TSituation'),'panel#situation_header').show();
