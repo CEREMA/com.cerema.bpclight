931e83a647db25cbf6392d79297a751af65b45eb
Stéphane Zucatti <stephane.zucatti@me.com>
2016-10-10 12:59:45 +0200

    Update# 1134

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -1029,7 +1029,7 @@ App.controller.define('CAgent', {
 		App.get(p,'TAgent textfield#Phone').setValue(p.agent.Telephone);
 		App.get(p,'TAgent textfield#Cell').setValue(p.agent.Portable);
 		
-		alert(p.agent._BLOB);
+		if (p.agent._BLOB=="") p.agent._BLOB=[];
 		App.get('TAgentPanel uploadfilemanager#up').setFiles(JSON.parse(p.agent._BLOB));
 		
 		// App.Agents.getAdresse
