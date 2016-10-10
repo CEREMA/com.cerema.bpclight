a24abb1ed47313b1f119546042bf23b452e5926a
Stéphane Zucatti <stephane.zucatti@me.com>
2016-10-10 12:26:51 +0200

    Update# 1128

--- src/System/app.js
+++ src/System/app.js
@@ -19,7 +19,18 @@ Math.uuid = function() {
 App = {
 	init: function(app,server) {
 		if (!require('fs').existsSync(__dirname + require('path').sep+'tmp')) require('fs').mkdirSync(__dirname + require('path').sep+'tmp');
-		//app.use('/tmp',server.static(__dirname + require('path').sep+'tmp'));
+		app.get('/docs/*',function(req,res) {
+			var ff=req.originalUrl.split('/docs/')[1];
+			App.using('db').query('bpclight','select * from docs where docId="'+ff+'"',function(err,response) {
+				if (response.length>0) {
+					if (response[0]._blob=="") {
+						res.end('Aucun document lié.');
+					} else {
+                        App.file.reader(response[0],res);
+					}
+				} else App.upload.reader(ff,res);
+            });
+        });		
 		app.post('/',app.upload.any(),function(req,res,next) {
 			App.upload.up(req,res);	
 		});
