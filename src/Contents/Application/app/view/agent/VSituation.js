80a166297a04f769b083e87670c9453d8c3650c6
Stéphane Zucatti <stephane.zucatti@me.com>
2016-10-25 15:53:43 +0200

    Update# 1230

--- src/Contents/Application/app/view/agent/VSituation.js
+++ src/Contents/Application/app/view/agent/VSituation.js
@@ -121,7 +121,7 @@ App.view.define('agent.VSituation', {
 						}	
 					}],
 					padding: 10,
-					store: App.store.create("bpclight://situadm"),
+					store: App.store.create("bpclight://situadm",{autoLoad: true}),
 					flex: 2,
 					height: 200
 				},
