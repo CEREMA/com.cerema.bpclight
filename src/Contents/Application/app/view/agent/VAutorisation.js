0bccd559e9b2ad911b72a7aef6a634b03ea735a9
Stéphane Zucatti <stephane.zucatti@me.com>
2016-10-25 09:16:26 +0200

    Update# 1154

---
+++ src/Contents/Application/app/view/agent/VAutorisation.js
@@ -0,0 +1,13 @@
+App.view.define('VAutorisation',{
+	extend: "Ext.Panel",
+	alias: 'widget.TAutorisation',
+	initComponent: function()
+	{
+		this.title = "Autorisation";
+		this.border= false;
+		this.layout="vbox";
+		this.items = [
+		];
+		this.callParent();
+	}
+})
