72ac338e7d9ddc3ebfe35630f615e3f46b7cd8c8
Stéphane Zucatti <stephane.zucatti@me.com>
2016-10-25 15:05:34 +0200

    Update# 1200

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -333,15 +333,16 @@ App.controller.define('CMain', {
 			});
 		} else {
 			App.DB.post("bpclight://agents",o,function(r) {
-				console.log(r);
-				App.Agents.getOne(r.insertId,function(e,m) {
-					console.log(m);
-					App.view.create('VAgentPanel',{
-						agent: m.result[0],
-						modal: true
-					}).show();
-					_p.up('window').close();
-				});
+				if (!r.insertId) alert("L'enregistrement a échoué.") else {
+					App.Agents.getOne(r.insertId,function(e,m) {
+						console.log(m);
+						App.view.create('VAgentPanel',{
+							agent: m.result[0],
+							modal: true
+						}).show();
+						_p.up('window').close();
+					});
+				};
 			});
 		}
 	},
