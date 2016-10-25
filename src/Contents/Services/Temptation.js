c3e2c994527ad2d72cc94b8cd2d0118b628c20ab
Stéphane Zucatti <stephane.zucatti@me.com>
2016-10-25 10:45:22 +0200

    Update# 1182

--- src/Contents/Services/Temptation.js
+++ src/Contents/Services/Temptation.js
@@ -5,10 +5,20 @@ Temptation = {
 		db.query('temptation',"SELECT MATRI matri, NOMPRE nompre, datouv FROM HopeMpl Hm where YEAR(datouv)>=YEAR(GetDate()) and datclo='1900-01-01' order by datouv desc",cb);
 	},
 	quotite: function(str,cb) {
-		Temptation.using('db').query('temptation',"SELECT MATRI, INETP,DAT FROM dbo.hophjoup WHERE MATRI LIKE '"+str+"' ORDER BY DAT",function(e,r){
-			console.log(e);
-			console.log(r);
-			cb();
+		Temptation.using('db').query('temptation',"SELECT MATRI, INETP,DAT FROM dbo.hophjoup WHERE MATRI LIKE '"+str+"' ORDER BY DAT desc",function(e,r){
+			var quotite="0";
+			var histo=[];
+			for (var i=0;i<r.length;i++) {
+				var item=r[i];
+				if (item.INETP!=quotite) {
+					quotite=item.INETP;
+					histo.push({
+						quotite: quotite,
+						date: item.DAT
+					});
+				}
+			}
+			cb(histo);
 		})
 	}
 };
