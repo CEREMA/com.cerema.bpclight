53e7c55c6749e553f6e596f8c58ea208e2c56de4
Stéphane Zucatti <stephane.zucatti@me.com>
2016-10-25 15:01:21 +0200

    Update# 1199

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -333,6 +333,7 @@ App.controller.define('CMain', {
 			});
 		} else {
 			App.DB.post("bpclight://agents",o,function(r) {
+				console.log(r);
 				App.Agents.getOne(r.insertId,function(e,m) {
 					console.log(m);
 					App.view.create('VAgentPanel',{
