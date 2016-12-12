2f76f442db02f5fea5db681f30b109010d8a9a43
Stéphane Zucatti <stephane.zucatti@me.com>
2016-12-12 18:08:11 +0100

    Update# 1310

--- src/System/app.js
+++ src/System/app.js
@@ -61,6 +61,7 @@ App = {
 					cc=cc.replace('#NOMPRENOM',item.Nom+' '+item.Prenom);
 					cc=cc.replace('#NUM',year+'-'+item.kage);
 					cc=cc.replace('#AFFECTATION',item.LibUnic+'/'+item.LibSubC);
+					cc=cc.replace('#CAT_PERMIS',item.CatPerm);
 					cc=cc.replace('#PERMIS',item.NumPie);
 					cc=cc.replace('#DATE_DEBUT','01/01/'+yearplus);
 					cc=cc.replace('#DATE_FIN','31/12/'+yearplus);
