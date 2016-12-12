aa00267324ed0bd4883f7ea0b8e0540e9145e252
Stéphane Zucatti <stephane.zucatti@me.com>
2016-12-12 17:36:27 +0100

    Update# 1294

--- src/System/app.js
+++ src/System/app.js
@@ -36,7 +36,7 @@ App = {
 		});
 		app.use('/css',server.static(__dirname+require('path').sep+'autorisations'+require('path').sep));
 		app.get('/report',function(req,res){
-			var html=require('fs').readFileSync(server.static(__dirname+require('path').sep+'autorisations'+require('path').sep+'index.html');
+			var html=require('fs').readFileSync(server.static(__dirname+require('path').sep+'autorisations'+require('path').sep+'index.html';
 			res.end(html);									
 		});
 		app.post('/agent',function(req,res) {
