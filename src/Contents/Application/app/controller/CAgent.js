50f07d02b52145b5ecc482adc6c8b5edd3b0c94f
Stéphane Zucatti <stephane.zucatti@me.com>
2016-12-12 11:36:18 +0100

    Update# 1255

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -129,7 +129,7 @@ App.controller.define('CAgent', {
 				show: function(p) {
 					// Autorisations
 					var store='bpclight://agepiece?TypPie=1&kage='+p.up('window').agent+'&kcpe=2';
-					App.get(p,'TAutorisation grid').bindStore(store);
+					App.get(p,'grid').bindStore(store);
 					store.load();
 				}	
 			},
