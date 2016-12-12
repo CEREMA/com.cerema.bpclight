21d584a9cfe766d29540ca4e46015adc10571597
Stéphane Zucatti <stephane.zucatti@me.com>
2016-12-12 18:11:54 +0100

    Update# 1314

--- src/System/app.js
+++ src/System/app.js
@@ -63,7 +63,7 @@ App = {
 					cc=cc.replace('#AFFECTATION',item.LibUnic+'/'+item.LibSubC);
 					cc=cc.replace('#CAT_PERMIS',item.CatPerm);
 					cc=cc.replace('#PERMIS',item.NumPie);
-					cc=cc.replace('#DATE_PERMIS',item.DatPie.toString('d-MMM-yyyy'));
+					cc=cc.replace('#DATE_PERMIS',item.DatPie.toLongDateString());
 					cc=cc.replace('#DATE_DEBUT','01/01/'+yearplus);
 					cc=cc.replace('#DATE_FIN','31/12/'+yearplus);
 					tpl.push(cc);
