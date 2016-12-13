123083677f7d9508ef952d9519a8206f0107f205
Stéphane Zucatti <stephane.zucatti@me.com>
2016-12-13 15:36:52 +0100

    Update# 1362

--- src/System/app.js
+++ src/System/app.js
@@ -72,7 +72,7 @@ App = {
 				require('fs').writeFileSync(tmp.path,html); 
 				var wkhtmltopdf = App.using('wkhtmltopdf');
 				var out=App.temp('pdf');
-				var stream = wkhtmltopdf(fs.createReadStream(out.path));
+				var stream = wkhtmltopdf(require('fs').createReadStream(out.path));
 				wkhtmltopdf(req.protocol+'://'+req.headers.host + tmp.url,{ pageSize: 'A4',dpi:390 }).pipe(stream).on('finish',function() {
 					res.end(out.url);
 				});
