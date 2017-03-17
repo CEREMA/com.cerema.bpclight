a318ddd02e96ebce92727dbeef2a5c597feaf6ee
Stéphane Zucatti <stephane.zucatti@me.com>
2017-03-17 08:58:25 +0100

    Update# 1412

--- src/Contents/Services/Medical.js
+++ src/Contents/Services/Medical.js
@@ -5,7 +5,7 @@ Medical={
 	},
 	printme: function(o,cb)
 	{
-		Medical.using('db').query('bpclight','select medic_dossiers.*,medic_type.*,medic_cat.*,agents.* from medic_dossiers join medic_type on medic_type.id=medic_dossiers.type join medic_cat on medic_cat.id=medic_dossiers.cat join agents on agents.kage=medic_dossiers.kage where agents.kage='+o.kage+' order by medic_dossiers.date desc',function(e,r){
+		Medical.using('db').query('bpclight','select medic_dossiers.*,medic_type.*,medic_cat.*,agents.* from medic_dossiers left join medic_type on medic_type.id=medic_dossiers.type left join medic_cat on medic_cat.id=medic_dossiers.cat left join agents on agents.kage=medic_dossiers.kage where agents.kage='+o.kage+' order by medic_dossiers.date desc',function(e,r){
 			console.log(r);
 			var html=[
 				"<html>",
