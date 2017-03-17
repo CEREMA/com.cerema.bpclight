47681b5eb936472881804d3b282112e2bbec4ec2
Stéphane Zucatti <stephane.zucatti@me.com>
2017-03-17 10:13:29 +0100

    Update# 1449

--- src/Contents/Services/Medical.js
+++ src/Contents/Services/Medical.js
@@ -23,6 +23,7 @@ Medical={
 				html.push('<h1>'+r.Nom+" "+r.Prenom+'</h1>');
 				html.push('<div class="commentaire">Type de visite : '+r.type+'</div>');
 				html.push('<div class="commentaire">Catégorie : '+r.cat+'</div>');
+				html.push('<div class="commentaire">Date : '+r.date+'</div>');
 				html.push('<h2>Poste actuel</h2><hr noshade>');
 				html.push('<div class="commentaire">'+r.poste_actuel+'</div>');
 				html.push('<h2>Antécédents personnels</h2><hr noshade>');
