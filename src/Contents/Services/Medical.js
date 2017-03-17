59ba0fa69778ba780f241450c0791e26f48128d2
Stéphane Zucatti <stephane.zucatti@me.com>
2017-03-17 09:08:42 +0100

    Update# 1419

--- src/Contents/Services/Medical.js
+++ src/Contents/Services/Medical.js
@@ -8,6 +8,7 @@ Medical={
 		Medical.using('db').query('bpclight','select medic_dossiers.*,medic_type.*,medic_cat.*,agents.* from medic_dossiers left join medic_type on medic_type.id=medic_dossiers.type left join medic_cat on medic_cat.id=medic_dossiers.cat left join agents on agents.kage=medic_dossiers.kage where agents.kage='+o.kid+' order by medic_dossiers.date desc',function(e,r){
 			console.log(e);
 			console.log(r);
+			r=r[0];
 			var html=[
 				"<html>",
 				"<head>",
