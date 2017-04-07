bb866d6e911d4aae99549c6fd5411c84057ded05
Stéphane Zucatti <stephane.zucatti@me.com>
2017-04-07 11:24:19 +0200

    Update# 1500

--- src/System/app.js
+++ src/System/app.js
@@ -272,7 +272,6 @@ App = {
 			if (req.body.kuni)
 			{
 				App.using('db').model('bpclight','SELECT batiments.LibBatC, unites.Kets KEts, batiments.GPS, agents.* FROM ((bpclight.agents agents LEFT OUTER JOIN bpclight.subdis subdis ON (agents.Ksub = subdis.Ksub)) LEFT OUTER JOIN bpclight.batiments batiments ON (agents.Kbat = batiments.Kbat)) LEFT OUTER JOIN bpclight.unites unites ON (agents.Kuni = unites.Kuni) WHERE actif=1 and unites.kuni='+req.body.kuni+' order by nom,prenom',function(err,o) {
-					console.log('zzzzzzzzzzz');
 					res.end(JSON.stringify(o,null,4));
 				});
 				return;
