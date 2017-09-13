8ff83d7826f015afe62ee658b48aae8afd542f33
Stéphane Zucatti <stephane.zucatti@me.com>
2017-09-13 11:10:29 +0200

    Update# 1524

--- src/Contents/Application/app/view/agent/VInformatique.js
+++ src/Contents/Application/app/view/agent/VInformatique.js
@@ -2,6 +2,13 @@ App.view.define('agent.VInformatique', {
     alias: "widget.TInformatique",
     extend: "Ext.Panel",
     initComponent: function() {
-	
+        this.title = "Informatique";
+        this.layout = "vbox";
+        this.border = false;
+        this.height="100%";
+        this.items = [
+		
+		];
+		this.callParent();
 	}
 });
